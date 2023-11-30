986cb172228173842f57bb3aeb737f532a712245
Hastypickaxe <hastypickaxe@gmail.com>
2023-11-29 22:14:02 -0800

    finish lab9

--- CS8Lab9/node2.h
+++ CS8Lab9/node2.h
@@ -9,99 +9,114 @@
 
 using namespace std;
 
-namespace template_bag4
-{
-    template <class Item>
-    class node
-    {
+namespace template_bag4 {
+    template<class Item>
+    class node {
     public:
-        node(const Item& init_data = Item(), node* init_link = NULL, node* init_prev = NULL) {
+        node(const Item &init_data = Item(), node *init_link = NULL, node *init_prev = NULL) {
             data_field = init_data;
             next = init_link;
             prev = init_prev;
         }
-        Item& data() { return data_field; }
-        node* getNext() { return next; }
-        node* getPrev() { return prev; }
-        void set_data(const Item& new_data) { data_field = new_data; }
-        void setNext(node* new_link) { next = new_link; }
-        void setPrev(node* new_link) { prev = new_link; }
-        const Item& data() const { return data_field; }
-        const node* getNext() const { return next; }
-        const node* getPrev() const { return prev; }
+
+        Item &data() { return data_field; }
+
+        node *getNext() { return next; }
+
+        node *getPrev() { return prev; }
+
+        void set_data(const Item &new_data) { data_field = new_data; }
+
+        void setNext(node *new_link) { next = new_link; }
+
+        void setPrev(node *new_link) { prev = new_link; }
+
+        const Item &data() const { return data_field; }
+
+        const node *getNext() const { return next; }
+
+        const node *getPrev() const { return prev; }
+
     private:
         Item data_field;
-        node* next;
-        node* prev;
+        node *next;
+        node *prev;
     };
 
-    template <class Item>
-    void list_clear(node<Item>*& head_ptr);
-    template <class Item>
-    void list_copy (const node<Item>* source_ptr, node<Item>*& head_ptr, node<Item>*& tail_ptr);template <class Item>
-    void list_head_insert(node<Item>*& head_ptr, const Item& entry);
-    template <class Item>
-    void list_head_remove(node<Item>*& head_ptr);
-    template <class Item>
-    void list_insert(node<Item>* previous_ptr, const Item& entry);
-    template <class Item>
-    void list_sort_insert(node<Item>*& head_ptr, const Item& entry);
-    template <class Item>
-    std::size_t list_length(const node<Item>* head_ptr);
-    template <class Item>
-    node<Item>* list_locate(node<Item>* head_ptr, int position);
-    template <class Item>
-    void list_remove(node<Item>* previous_ptr);
-    template <class Item>
-    node<Item>* list_search(node<Item>* head_ptr, const Item& target);
-    template <class Item>
-    void list_display(node<Item>* head);
+    template<class Item>
+    void list_clear(node<Item> *&head_ptr);
 
-    template <class Item>
-    void list_clear(node<Item>*& head_ptr) {
+    template<class Item>
+    void list_copy(const node<Item> *source_ptr, node<Item> *&head_ptr, node<Item> *&tail_ptr);
+
+    template<class Item>
+    void list_head_insert(node<Item> *&head_ptr, const Item &entry);
+
+    template<class Item>
+    void list_head_remove(node<Item> *&head_ptr);
+
+    template<class Item>
+    void list_insert(node<Item> *previous_ptr, const Item &entry);
+
+    template<class Item>
+    void list_sort_insert(node<Item> *&head_ptr, const Item &entry);
+
+    template<class Item>
+    std::size_t list_length(const node<Item> *head_ptr);
+
+    template<class Item>
+    node<Item> *list_locate(node<Item> *head_ptr, int position);
+
+    template<class Item>
+    void list_remove(node<Item> *previous_ptr);
+
+    template<class Item>
+    node<Item> *list_search(node<Item> *head_ptr, const Item &target);
+
+    template<class Item>
+    void list_clear(node<Item> *&head_ptr) {
         while (head_ptr != NULL)
             list_head_remove(head_ptr);
     }
-    template <class Item>
-    void list_copy(const node<Item>* source_ptr, node<Item>*& head_ptr, node<Item>*& tail_ptr) {head_ptr = tail_ptr = NULL;
+
+    template<class Item>
+    void list_copy(const node<Item> *source_ptr, node<Item> *&head_ptr, node<Item> *&tail_ptr) {
+        head_ptr = tail_ptr = NULL;
         if (source_ptr == NULL) return;
 // Make the head node for the newly created list, and put data in itlist_head_insert(head_ptr, source_ptr->data());
         tail_ptr = head_ptr;
 // Copy rest of the nodes one at a time, adding at the tail of new listsource_ptr = source_ptr->getNext();
-        while (source_ptr != NULL)
-        {
+        while (source_ptr != NULL) {
             list_insert(tail_ptr, source_ptr->data());
             tail_ptr = tail_ptr->getNext();
             source_ptr = source_ptr->getNext();
         }
     }
 
-    template <class Item>
-    void list_head_insert(node<Item>*& head_ptr, const Item& entry)
-    {
+    template<class Item>
+    void list_head_insert(node<Item> *&head_ptr, const Item &entry) {
         head_ptr = new node<Item>(entry, head_ptr);
     }
-    template <class Item>
-    void list_head_remove(node<Item>*& head_ptr)
-    {
-        node<Item>* remove_ptr;
+
+    template<class Item>
+    void list_head_remove(node<Item> *&head_ptr) {
+        node<Item> *remove_ptr;
         remove_ptr = head_ptr;
         head_ptr = head_ptr->getNext();
         delete remove_ptr;
     }
-    template <class Item>
-    void list_insert(node<Item>* previous_ptr, const Item& entry)
-    {
-        node<Item>* insert_ptr;
+
+    template<class Item>
+    void list_insert(node<Item> *previous_ptr, const Item &entry) {
+        node<Item> *insert_ptr;
         insert_ptr = new node<Item>(entry, previous_ptr->getNext());
         previous_ptr->setNext(insert_ptr);
         insert_ptr->setPrev(previous_ptr);
     }
 
-    template <class Item>
-    void list_sort_insert(node<Item>*& head_ptr, const Item& entry)
-    {
-        node<Item>* newNode;
+    template<class Item>
+    void list_sort_insert(node<Item> *&head_ptr, const Item &entry) {
+        node<Item> *newNode;
         newNode = new node<Item>(entry);
 
         if (head_ptr->data() >= newNode->data()) {
@@ -109,7 +124,7 @@ namespace template_bag4
             newNode->getNext()->setPrev(newNode);
             head_ptr = newNode;
         } else {
-            node<Item>* current = head_ptr;
+            node<Item> *current = head_ptr;
 
             while (current->getNext() != NULL && current->getNext()->data() < newNode->data()) {
                 current = current->getNext();
@@ -126,19 +141,18 @@ namespace template_bag4
         }
     }
 
-    template <class Item>
-    std::size_t list_length(const node<Item>* head_ptr)
-    {
-        const node<Item>* cursor;
+    template<class Item>
+    std::size_t list_length(const node<Item> *head_ptr) {
+        const node<Item> *cursor;
         std::size_t answer;
         answer = 0;
         for (cursor = head_ptr; cursor != NULL; cursor = cursor->getNext())++answer;
         return answer;
     }
-    template <class Item>
-    node<Item>* list_locate(node<Item>* head_ptr, int position)
-    {
-        node<Item>* cursor;
+
+    template<class Item>
+    node<Item> *list_locate(node<Item> *head_ptr, int position) {
+        node<Item> *cursor;
         int i;
         assert(0 < position);
         cursor = head_ptr;
@@ -147,10 +161,9 @@ namespace template_bag4
         return cursor;
     }
 
-    template <class Item>
-    void list_remove(node<Item>* previous_ptr)
-    {
-        node<Item>* remove_ptr;
+    template<class Item>
+    void list_remove(node<Item> *previous_ptr) {
+        node<Item> *remove_ptr;
         remove_ptr = previous_ptr->getNext();
         previous_ptr->setNext(remove_ptr->getNext());
         if (previous_ptr->getNext() != NULL) {
@@ -158,25 +171,15 @@ namespace template_bag4
         }
         delete remove_ptr;
     }
-    template <class Item>
-    node<Item>* list_search(node<Item>* head_ptr, const Item& target)
-    {
-        node<Item>* cursor;
-        for (cursor = head_ptr; cursor != NULL; cursor = cursor->getNext())if (target == cursor->data())
+
+    template<class Item>
+    node<Item> *list_search(node<Item> *head_ptr, const Item &target) {
+        node<Item> *cursor;
+        for (cursor = head_ptr; cursor != NULL; cursor = cursor->getNext())
+            if (target == cursor->data())
                 return cursor;
         return NULL;
     }
 
-    template <class Item>
-    void list_display(node<Item>*& head) {
-        node<Item>* cursor = head;
-
-        while (cursor != NULL) {
-            cout << cursor->data() << "<->";
-            cursor = cursor->getNext();
-        }
-
-        cout << "NULL" << endl;
-    }
-
-#endif //CS8LAB9_NODE2_H
+}
+#endif // CS8LAB9_NODE2_H
